6361422efb0491dcb230189a556c51ccca8b93c9
Zainab Naveed <zainabnaveed199@gmail.com>
2024-11-15 12:17:04 +0500

    original commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import {useState} from 'react';
+
+export default function Counter() {
+  const[score, scoreSet] = useState(0);
+
+  function increment(){
+   scoreSet(score+1);
+  }
+  function increment5(){
+    scoreSet(score+5);
+    }
+
+  function decrement(){
+    if (score > 0){
+    scoreSet(score-1);
+    }
+   }
+  function decrement5(){
+    if (score > 4){
+    scoreSet(score-5);
+    }
+  }
+function reset(){
+  scoreSet(0);
 }
-
-export default App;
+return (
+    <div className="flex bg-pink-900 h-[100vh] text-white items-center justify-center flex-wrap">
+      <div>
+    <div className="bg-pink-800 w-[100%] h-[20px]">
+    <h1 className="bg-pink-600 p-1 pl-12 text-2xl" >COUNTER</h1>
+    <div className="flex w-[100%] ">
+    <button className="p-2 m-1 bg-pink-700 items-center justify-center" onClick={increment5}>+5</button>
+   <button className="p-2 m-1 bg-pink-700 items-center justify-center" onClick={increment}>+1</button>
+<p className="p-2 bg-pink-600 m-1">score: {score}</p> 
+ <button className="p-2 bg-pink-700 m-1" onClick={decrement}>-1</button>
+   <button className="p-2 m-1 bg-pink-700 items-center justify-center" onClick={decrement5} disabled={scoreSet<5}>-5</button>
+   <button className="p-2 m-1 bg-pink-700 items-center justify-center" onClick={reset}>Reset</button>
+</div>
+   </div>
+   </div>
+   </div>
+);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',  // Tailwind will purge unused CSS from these files
+     ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
